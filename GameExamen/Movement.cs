26e9654ae54da8acb0af0621c8067ba3afa141f1
Dubrovskiy-Andrey <144478406+Dubrovskiy-Andrey@users.noreply.github.com>
2026-02-10 09:28:18 +0500

    Add files via upload

---
+++ GameExamen/Movement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameExamen
+{
+    internal class Movement
+    {
+        static void Sprint()
+        {
+            Console.WriteLine("Пользователь бежит");
+        }
+    }
+}
